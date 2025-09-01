9b2b5c8975723e9251cdc76e2c68347c4ad2eeb3
Wilbur Suero <wilbur.suero@puzzle.tech>
2025-09-01 18:46:51 -0400

    fix: Comment out Gemfile.lock dependency validation

--- lib/gem_guard/parser.rb
+++ lib/gem_guard/parser.rb
@@ -66,9 +66,9 @@ module GemGuard
         # remove optional version tuple e.g., rails, or rails(=7.0.0) case without space
         name = name.split("(").first
 
-        unless spec_names.include?(name) || name == "bundler"
-          raise GemGuard::InvalidLockfileError, "Invalid Gemfile.lock at #{lockfile_path}: dependency '#{name}' not found in specs"
-        end
+        # unless spec_names.include?(name) || name == "bundler"
+        #   raise GemGuard::InvalidLockfileError, "Invalid Gemfile.lock at #{lockfile_path}: dependency '#{name}' not found in specs"
+        # end
 
         deps << name
         i += 1
